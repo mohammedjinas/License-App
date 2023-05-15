0ae2151af3cd1cbfcc99cef0d00707255c33afef
mohammedjinas <mohammedjinas7972@gmail.com>
2023-05-15 14:31:22 +0400

    ios fixes

---
+++ lib/models/clientdet_search_model.dart
@@ -0,0 +1,7 @@
+class ClientDetSearchModel
+{
+  String name, customerId, noOfLicense, remarks;
+  
+
+  ClientDetSearchModel({required this.name, required this.customerId, required this.noOfLicense,required this.remarks});
+}
--- lib/screens/client_details.dart
+++ lib/screens/client_details.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:license/models/clientdet_search_model.dart';
 import 'package:license/widgets/license_expiry_list.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -43,7 +44,7 @@ class _ClientDetialsState extends State<ClientDetials> {
       child: Scaffold(appBar: AppBar(title: const Text("RetailX License"),
       actions: [
           IconButton(onPressed: () async {
-            ClientSearchModel selectedItem = await showSearch(context: context, delegate: MySearchDelegate());
+            ClientDetSearchModel selectedItem = await showSearch(context: context, delegate: MySearchDelegate());
             setState(() {
               customerId.text = selectedItem.customerId.toString();
               resultText.text = "";
@@ -214,7 +215,8 @@ class _ClientDetialsState extends State<ClientDetials> {
 
 class MySearchDelegate extends SearchDelegate
 {
-  List<ClientSearchModel> customerList = [];  
+  List<ClientDetSearchModel> customerList = [];  
+  TextEditingController txtRemarks = TextEditingController();
   
   double width = 0.0, height = 0.0; 
   @override
@@ -260,30 +262,16 @@ class MySearchDelegate extends SearchDelegate
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    
-        return Container();
-    // return FutureBuilder(future: searchCustomer(query,context),
-    //   builder: ((context, snapshot) {
-    //   if(snapshot.connectionState == ConnectionState.done)
-    //   {
-    //     if(snapshot.hasData)
-    //     return buildList(snapshot.data);
-    //     else return Container();
-    //   }
-    //   else
-    //   {
-    //     return Container();
-    //   }
-    // }));
+    return Container();
   }
 
-  Future<List<ClientSearchModel>?> searchCustomer(String searchText,BuildContext ctx) async
+  Future<List<ClientDetSearchModel>?> searchCustomer(String searchText,BuildContext ctx) async
   {
     if(searchText == "")
     {
       return null;
     }
-    List<ClientSearchModel> customerList = []; 
+    // List<ClientDetSearchModel> customerList = []; 
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? baseURL = prefs.getString("baseURL");
     final url = Uri.parse("$baseURL/Home/SearchCustomer/$searchText");
@@ -299,9 +287,10 @@ class MySearchDelegate extends SearchDelegate
           final List<dynamic> responseList = searchResult["clientList"];
           for(int i = 0; i < responseList.length; i++)
           {
-            ClientSearchModel obj = ClientSearchModel(name: responseList[i]["name"], customerId: responseList[i]["customerId"], noOfLicense: responseList[i]["noOfLicense"]);
+            ClientDetSearchModel obj = ClientDetSearchModel(name: responseList[i]["name"], customerId: responseList[i]["customerId"], noOfLicense: responseList[i]["noOfLicense"],remarks: responseList[i]["remarks"]);
             customerList.add(obj);
           }
+          
         }
         else
         {
@@ -323,7 +312,7 @@ class MySearchDelegate extends SearchDelegate
     return customerList;
   }
 
-  Widget buildList(List<ClientSearchModel>? clientList)
+  Widget buildList(List<ClientDetSearchModel>? clientList)
   {
    if(clientList!.isNotEmpty) return ListView.builder(itemBuilder: ((context, index) {
       
@@ -332,36 +321,39 @@ class MySearchDelegate extends SearchDelegate
       return InkWell(
         child: Container(
           height: height * 0.05,
-          width: width * 0.9,
-          decoration: BoxDecoration(color: Colors.blue[50]),
+          width: width * 0.5,
+          // decoration: BoxDecoration(color: Colors.blue[50]),
           alignment: Alignment.center,
           child:  FittedBox(
             fit: BoxFit.fitWidth, 
             child: Row(
               children: [
-                Text(clientList[index].name, 
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: height * 0.015),
-                  ),
-              //     SizedBox(height: height * 0.05,width: width * 0.1,
-              //   child: 
-              //   Ink.image(image:const AssetImage("assets/images/remarks.png"),
-              // child: InkWell(onTap: () {
-              //   showRemarksDialog(index,clientList[index].Remarks);
-              // },),),
-              //   // IconButton(onPressed: () {
-              //   //   deleteClient(widget.systemsList[index].id);
-              //   //   setState(() {widget.systemsList.removeAt(index);});
-              //   // }, 
-              //   // icon: const Icon(Icons.delete_forever,color: Colors.red,),),
-              //   ),
+                SizedBox(width: width * 0.7,
+                  child: 
+                  Text(clientList[index].name, 
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: height * 0.015,),
+                    ),
+                ),
+                  SizedBox(height: height * 0.05,width: width * 0.2 ,
+                child: 
+                Ink.image(image:const AssetImage("assets/images/remarks.png"),
+              child: InkWell(onTap: () {
+                showRemarksDialog(index,clientList[index].customerId,clientList[index].remarks,context);
+              },),),
+                // IconButton(onPressed: () {
+                //   deleteClient(widget.systemsList[index].id);
+                //   setState(() {widget.systemsList.removeAt(index);});
+                // }, 
+                // icon: const Icon(Icons.delete_forever,color: Colors.red,),),
+                ),
               ],
             )
             ),
           ),
           onTap: () {
-            ClientSearchModel selectedItem = ClientSearchModel(name: clientList[index].name, customerId: clientList[index].customerId, noOfLicense: clientList[index].noOfLicense);
+            ClientDetSearchModel selectedItem = ClientDetSearchModel(name: clientList[index].name, customerId: clientList[index].customerId, noOfLicense: clientList[index].noOfLicense,remarks: clientList[index].remarks);
             close(context, selectedItem);
           },
       );
@@ -371,46 +363,59 @@ class MySearchDelegate extends SearchDelegate
     {return Container();}
   }
 
-  // void showRemarksDialog(int index,String remarks)
-  // {
-  //   txtRemarks.text = remarks;
-  //   showDialog(barrierDismissible: true, context: context, builder: (context) {
-  //     return SingleChildScrollView(
-  //       child: Dialog(insetPadding: EdgeInsets.symmetric(vertical: height * 0.25, horizontal: width * 0.1), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-  //       child: Padding(padding: const EdgeInsets.all(10),
-  //       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-  //         Container(padding: const EdgeInsets.only(bottom: 20), 
-  //         child: Text("REMARKS", style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.bold, fontSize: width * 0.05),
-  //           ),),
+  void showRemarksDialog(int index,String id,String remarks,BuildContext context)
+  {
+    txtRemarks.text = remarks;
+    showDialog(barrierDismissible: true, context: context, builder: (context) {
+      return SingleChildScrollView(
+        child: Dialog(insetPadding: EdgeInsets.symmetric(vertical: height * 0.25, horizontal: width * 0.1), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Padding(padding: const EdgeInsets.all(10),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(padding: const EdgeInsets.only(bottom: 20), 
+          child: Text("REMARKS", style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.bold, fontSize: width * 0.05),
+            ),),
       
-  //         Container(padding: const EdgeInsets.only(bottom: 20), 
-  //         child: TextField(decoration: InputDecoration(labelText: "Remarks",labelStyle: TextStyle(color: Colors.grey[600]), focusColor: Colors.blue, 
-  //           border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),),),            
-  //           controller: txtRemarks,
-  //           minLines: 1,
-  //           maxLines: 5,
-  //         ),),
+          Container(padding: const EdgeInsets.only(bottom: 20), 
+          child: TextField(decoration: InputDecoration(labelText: "Remarks",labelStyle: TextStyle(color: Colors.grey[600]), focusColor: Colors.blue, 
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),),),            
+            controller: txtRemarks,
+            minLines: 1,
+            maxLines: 5,
+          ),),
 
-  //         Row(
-  //           children: [ ElevatedButton( style: ElevatedButton.styleFrom(backgroundColor: Colors.white,elevation: 20), child: Container(alignment: Alignment.center, width: width * 0.28, 
-  //               child: const Text("Cancel",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)),
-  //                         onPressed: () {Navigator.of(context).pop();},),
-  //             Padding(
-  //               padding: const EdgeInsets.only(left: 10.0),
-  //               child: ElevatedButton( style: ElevatedButton.styleFrom(backgroundColor: Colors.blue[100],elevation: 20), child: Container(alignment: Alignment.center, width: width * 0.28, 
-  //                 child: const Text("Save",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)),
-  //                 onPressed: () {
-  //                       saveRemarks(id);
-  //                       widget.systemsList[index].Remarks = txtRemarks.text;
-  //                       Navigator.of(context).pop();
-  //                   // }
-  //                 },),
-  //             ),
-  //           ],
-  //         ) 
+          Row(
+            children: [ ElevatedButton( style: ElevatedButton.styleFrom(backgroundColor: Colors.white,elevation: 20), child: Container(alignment: Alignment.center, width: width * 0.28, 
+                child: const Text("Cancel",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)),
+                          onPressed: () {Navigator.of(context).pop();},),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: ElevatedButton( style: ElevatedButton.styleFrom(backgroundColor: Colors.blue[100],elevation: 20), child: Container(alignment: Alignment.center, width: width * 0.28, 
+                  child: const Text("Save",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)),
+                  onPressed: () {
+                        saveRemarks(id);
+                        customerList[index].remarks = txtRemarks.text;
+                        Navigator.of(context).pop();
+                    // }
+                  },),
+              ),
+            ],
+          ) 
       
-  //       ],),),),
-  //     );
-  //    });
-  // }
+        ],),),),
+      );
+     });
+  }
+
+
+  void saveRemarks(String id) async
+  {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? baseURL = prefs.getString("baseURL");
+    final remarks = txtRemarks.text;
+    
+    final url = Uri.parse("$baseURL/Home/UpdateClientRemarks/$remarks,$id");
+    await get(url);
+
+    // setState(() {});
+  }
 }
